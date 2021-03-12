3b7dab0d5190acde6d3b54e735b58b58aa9a7b5a
andrey <serzantovandreyiq@gmail.com>
2021-03-12 15:22:23 +0300

    fix: fixed a bug in previously implemented functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 module.exports = function reverse (n) {
   let arr = [];
-        n = String(n);
+          n = String(n);
 
-    for(let i of n){
-        arr.push(i);
-    }
+          if(n < 0){
+            for(let i of n){
+                if(i == '-'){
+                    continue;
+                }else{
+                    arr.push(i);
+                }
+            }
+            arr.reverse();
+          }else{
+            for(let i of n){
+                arr.push(i);
+            }
+            arr.reverse();
+          }
 
-    return +arr.reverse().join('');
+          console.log(arr)
+  
+      return +arr.join('');;
 }
+
+
